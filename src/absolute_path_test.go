2ceb7a9c6ab7f245a73e74e1b0d1a717ea662318
rhysd <lin90162@yahoo.co.jp>
2016-09-15 17:41:33 +0900

    remove removed APIs' tests in absolute_path_test.go

--- src/absolute_path_test.go
+++ src/absolute_path_test.go
@@ -3,59 +3,9 @@ package dotfiles
 import (
 	"os"
 	"os/user"
-	"path"
 	"path/filepath"
-	"strings"
 	"testing"
 )
-
-func TestAbsolutePathNew(t *testing.T) {
-	a, err := NewAbsolutePath("/path/to/somewhere")
-	if err != nil {
-		t.Error(err)
-	}
-
-	if string(a) != "/path/to/somewhere" {
-		t.Errorf("Invalid absolute path: %s", a)
-	}
-
-	if !a.Compare("/path/to/somewhere") {
-		t.Errorf("Compare() must be used for comparing absolute path and string")
-	}
-}
-
-func TestAbsolutePathHomeDirExpansion(t *testing.T) {
-	a, err := NewAbsolutePath("~/foo")
-	if err != nil {
-		t.Error(err)
-	}
-	if !path.IsAbs(string(a)) {
-		t.Errorf("'~' must be converted to full home directory path: %s", a)
-	}
-	if !strings.HasSuffix(string(a), "/foo") {
-		t.Errorf("Invalid path conversion for home directory expansion: %s", a)
-	}
-}
-
-func TestAbsolutePathEmpty(t *testing.T) {
-	a, err := NewAbsolutePath("")
-	if err != nil {
-		t.Error(err)
-	}
-	if string(a) != "" {
-		t.Errorf("Path converted from empty path must be empty: '%s'", a)
-	}
-	if !a.IsEmpty() {
-		t.Errorf("IsEmpty() returns false when empty absolute path: '%s'", a)
-	}
-}
-
-func TestAbsolutePathRelativePathError(t *testing.T) {
-	_, err := NewAbsolutePath("relative-path")
-	if err == nil {
-		t.Errorf("NewAbsolutePath() must raise an error when relative path is given")
-	}
-}
 
 func TestAbsolutePathToRepo(t *testing.T) {
 	cwd, err := os.Getwd()
@@ -84,7 +34,7 @@ func TestAbsolutePathToRepo(t *testing.T) {
 		{"~", u.HomeDir},
 		{cwd, cwd},
 	} {
-		r, err := AbsolutePathToRepo(c.input)
+		r, err := absolutePathToRepo(c.input)
 		if err != nil {
 			t.Errorf("Unexpected error for input '%s': %s", c.input, err.Error())
 			continue
@@ -105,7 +55,7 @@ func TestAbsolutePathToRepo(t *testing.T) {
 		"unknown_dir",
 		"./existing_file",
 	} {
-		_, err := AbsolutePathToRepo(e)
+		_, err := absolutePathToRepo(e)
 		if err == nil {
 			t.Errorf("'%s' is an invalid value for repository but no error occurred", e)
 		}
@@ -120,7 +70,7 @@ func TestAbsolutePathToRepo(t *testing.T) {
 		{".", abs(".")},
 	} {
 		os.Setenv("DOTFILES_REPO_PATH", c.env)
-		r, err := AbsolutePathToRepo("")
+		r, err := absolutePathToRepo("")
 		if err != nil {
 			t.Errorf("Unexpected error for $DOEFILES_REPO_PATH '%s': %s", c.env, err.Error())
 			continue
